aeb97c455cd23d0ef5e05923dad8f9e0baa3e384
KYF8HC <gulyaspeter20@gmail.com>
2025-01-25 22:17:36 +0100

    Ground check

--- Assets/Scripts/BubbleLeague/Player/JumpMovement.cs
+++ Assets/Scripts/BubbleLeague/Player/JumpMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,18 +7,28 @@ namespace BubbleLeague.Player
     public class JumpMovement : MonoBehaviour
     {
         private Rigidbody m_rigidbody;
+        private bool m_isGrounded;
         [SerializeField] private GameObject m_sphere;
-        [SerializeField] protected float m_jumpForce = 10000.0f;
+        [SerializeField] private float m_jumpForce = 10000.0f;
+        [SerializeField] private LayerMask m_groundLayer;
+        [Range(0.01f, 1.0f)][SerializeField] private float m_groundCheckRadius = 0.2f;
 
         protected void Awake()
         {
             m_rigidbody = m_sphere.GetComponent<Rigidbody>();
         }
 
+        private void Update()
+        {
+            m_isGrounded = Physics.CheckSphere(transform.position, 0.7f, m_groundLayer);
+        }
+
         public void OnJump()
         {
-            Debug.Log("Jump!");
-            m_rigidbody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+            if (m_isGrounded)
+            {
+                m_rigidbody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+            }
         }
     }
 }
