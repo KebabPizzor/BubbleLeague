2570835dd9087b5fdd1cd4d4c0fd82e53f7ef9f3
Marc Zaku <mzaku@hotmail.de>
2025-01-25 22:27:06 +0100

    Improve Jump and Gravity Controls

--- Assets/Scripts/BubbleLeague/Player/JumpMovement.cs
+++ Assets/Scripts/BubbleLeague/Player/JumpMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +9,9 @@ namespace BubbleLeague.Player
         private Rigidbody m_rigidbody;
         [SerializeField] private GameObject m_sphere;
         [SerializeField] protected float m_jumpForce = 10000.0f;
+        [SerializeField] private float m_jumpFastFallDelay = 1f;
+        [SerializeField] private Vector3 m_fastFallGravity = new Vector3(0f, -10f, 0f);
+        private float m_timeWithoutFastFall;
 
         protected void Awake()
         {
@@ -17,7 +21,17 @@ namespace BubbleLeague.Player
         public void OnJump()
         {
             Debug.Log("Jump!");
-            m_rigidbody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+            m_rigidbody.AddForce(Vector3.up * m_jumpForce, ForceMode.Acceleration);
+            m_timeWithoutFastFall = m_jumpFastFallDelay;
+        }
+
+        private void FixedUpdate()
+        {
+            if (m_timeWithoutFastFall > 0f)
+            {
+                m_timeWithoutFastFall -= Time.fixedDeltaTime;
+                m_rigidbody.AddForce(m_fastFallGravity, ForceMode.Acceleration);
+            }
         }
     }
 }
